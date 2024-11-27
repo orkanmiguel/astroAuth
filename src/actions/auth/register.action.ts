14deb15d43c907c23c1b32fe69e7edd4d717666f
orkanmiguel <orkan.miguel@gmail.com>
2024-11-27 13:27:20 -0300

    register Actions

---
+++ src/actions/auth/register.action.ts
@@ -0,0 +1,16 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+
+export const registerUser = defineAction({
+  accept: "form",
+  input: z.object({
+    name: z.string().min(2),
+    email: z.string().email(),
+    password: z.string().min(6),
+    remember_me: z.boolean().optional(),
+  }),
+  handler: async ({ name, password, remember_me, email }) => {
+    console.log({ name, password, remember_me, email });
+    return true;
+  },
+});

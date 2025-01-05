0a9f5129fe47b6a3f7f513740f57d8dbacbda3a5
orkanmiguel <orkan.miguel@gmail.com>
2025-01-05 00:08:01 -0300

    add restrictions

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,17 @@
 /// <reference path="../.astro/types.d.ts" />
+
+import type { boolean } from "astro:schema";
+
+interface User {
+  email: string;
+  name: string;
+  avatar: string;
+  emailVerified: Boolean;
+}
+
+declare namespace App {
+  interface Locals {
+    isLoggedIn: boolean;
+    user: User | null;
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,34 @@
 import { defineMiddleware } from "astro:middleware";
+import { firebase } from "./firebase/config";
+
+const privateRoutes = ["/protected"];
+const notAuthenticatedRoutes = ["/login", "/register"];
 
 // `context` and `next` are automatically typed
-export const onRequest = defineMiddleware((context, next) => {
-  return next();
-});
+export const onRequest = defineMiddleware(
+  async ({ url, request, locals, redirect }, next) => {
+    const isLoggedIn = !!firebase.auth.currentUser;
+
+    const user = firebase.auth.currentUser;
+    locals.isLoggedIn = isLoggedIn;
+
+    if (user) {
+      locals.user = {
+        avatar: user.photoURL ?? "",
+        email: user.email!,
+        name: user.displayName!,
+        emailVerified: user.emailVerified,
+      };
+    }
+
+    if (!isLoggedIn && privateRoutes.includes(url.pathname)) {
+      return redirect("/");
+    }
+
+    if (isLoggedIn && notAuthenticatedRoutes.includes(url.pathname)) {
+      return redirect("/");
+    }
+
+    return next();
+  }
+);
